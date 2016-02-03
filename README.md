adcded4f6a1fbd6cffe115d8ec9c9fb4cb44e586
Kunal Bhat <kunalbhat@gmail.com>
2016-02-02 19:12:45 -0600

    Update readme and switch rb for py on 3

--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 Project Euler is a series of challenging mathematical/computer programming
 problems that will require more than just mathematical insights to solve.
 
-### Intent
-Personal endeavor to learn Ruby more idiomatically.
-
 [projecteuler.net](http://projecteuler.net/)
---
+++ e003.py
@@ -0,0 +1,43 @@
+# Largest prime factor
+# Problem 3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143?
+
+def multiply_factors(list, num):
+    product = 1
+
+    for i in list:
+        product *= i
+
+    if product == num:
+        print(list)
+
+def check_prime_factors(num):
+    factors_list = get_factors(num)
+
+    #Debug
+    print factors_list
+
+    list_length = len(factors_list)
+    i = list_length
+    factors_subset = []
+
+    while i > 0:
+        factors_subset.append(factors_list[i-1])
+        i -= 1
+
+        multiply_factors(factors_subset, num)
+
+def get_factors(num):
+    original_num = num
+    factors = []
+
+    while num > 0:
+        if original_num % num == 0:
+            factors.append(num)
+
+        num -= 1
+
+    return factors
+
+check_prime_factors(501240)
