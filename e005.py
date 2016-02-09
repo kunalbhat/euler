a537c8f4e1cc9a4d57042de5ad62a3870b4a051d
Kunal Bhat <kunalbhat@gmail.com>
2016-02-08 22:14:48 -0600

    Gets the solution, next optimize

--- e005.py
+++ e005.py
@@ -7,4 +7,24 @@
 # What is the smallest positive number that is evenly divisible by all of the
 # numbers from 1 to 20?
 
+def get_multiple():
+    start_num = 2520
+    max_num = 21
+
+    while start_num >= 2520:
+        z = []
+        for i in range(1,max_num):
+            if start_num % i != 0:
+                break
+            else:
+                z.append(start_num % i)
+                continue
+
+        if z.count(0) == max_num-1:
+            print(start_num, z, len(z))
+            break
+
+        start_num += 2520
+
+get_multiple()
 
